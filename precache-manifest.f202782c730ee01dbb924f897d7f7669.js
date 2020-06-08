32c7f0f15c909694f83b406df08813d8efd49d28
Ishaan Nerurkar <ishaan.ner@gmail.com>
2020-06-08 14:49:35 +1000

    Updates

---
+++ precache-manifest.f202782c730ee01dbb924f897d7f7669.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d51fb5abda7ebdabdcb0c6da81096ef",
+    "url": "/resume-builder/index.html"
+  },
+  {
+    "revision": "e1e6c0d84796b39c2d8d",
+    "url": "/resume-builder/static/css/main.87e69abf.chunk.css"
+  },
+  {
+    "revision": "5706cb4711f8c7a361e4",
+    "url": "/resume-builder/static/js/2.d68158b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/resume-builder/static/js/2.d68158b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e6c0d84796b39c2d8d",
+    "url": "/resume-builder/static/js/main.9c3ab69d.chunk.js"
+  },
+  {
+    "revision": "5edbe3c530fb7f63adf5",
+    "url": "/resume-builder/static/js/runtime-main.4092ad16.js"
+  },
+  {
+    "revision": "9851403b40b481ab3dadb8a16ed3878e",
+    "url": "/resume-builder/static/media/template1.9851403b.jpg"
+  },
+  {
+    "revision": "7e6b674e6902aa08a8ac5dd0f14a22ca",
+    "url": "/resume-builder/static/media/template2.7e6b674e.jpg"
+  },
+  {
+    "revision": "49cc75662a6f20dfdfc8a522aef352b2",
+    "url": "/resume-builder/static/media/template3.49cc7566.jpg"
+  }
+]);
